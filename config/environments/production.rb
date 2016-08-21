550a60bf571937af955472a60f2568ede526f6ec
Yaroslav Smirnov <stellarnode@ya.ru>
2016-08-21 12:36:41 +0300

    updated dev and prod configs to allow origin for js request in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = ['http://localhost:3000', ENV["ROOT_URL"], ENV["ROOT_URL_PROD"]]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
