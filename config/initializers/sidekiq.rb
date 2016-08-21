480dd51948ee38a9005e086025c9e8e3976d8908
Yaroslav Smirnov <stellarnode@ya.ru>
2016-08-21 12:20:52 +0300

    fixed typo bug in sidekiq initializer to correctly use env var

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,12 +1,12 @@
-# if ENV["RAILS-ENV"] == "production"
-#   redis_url = ENV['REDIS_PROD']
-#
-#   Sidekiq.configure_server do |config|
-#     config.redis = { url: redis_url }
-#   end
-#
-#   Sidekiq.configure_client do |config|
-#     config.redis = { url: redis_url }
-#   end
-#
-# end
+if ENV["RAILS-ENV"] == "production"
+  redis_url = ENV['REDIS-PROD']
+
+  Sidekiq.configure_server do |config|
+    config.redis = { url: redis_url }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: redis_url }
+  end
+
+end
