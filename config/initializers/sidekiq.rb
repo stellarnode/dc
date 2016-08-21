457fa020d2c32a38b7e12a9460d0a591d1753649
Yaroslav Smirnov <stellarnode@ya.ru>
2016-08-21 12:13:46 +0300

    updated sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,17 +1,12 @@
-if defined? Sidekiq && ENV["RAILS-ENV"] == "production"
+if ENV["RAILS-ENV"] == "production"
   redis_url = ENV['REDIS_PROD']
 
   Sidekiq.configure_server do |config|
-    config.redis = {
-      :url => redis_url
-    }
+    config.redis = { url: redis_url }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = {
-      :url => redis_url
-    }
+    config.redis = { url: redis_url }
   end
-
 
 end
