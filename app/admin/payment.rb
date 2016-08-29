dd9d1952a2bdcac6d85aac00d9724f9de4596425
Almazzzzz <badrtdinov_a@mail.ru>
2016-08-29 12:40:16 +0300

    fix: change payment index in admin part

--- app/admin/payment.rb
+++ app/admin/payment.rb
@@ -7,7 +7,20 @@ ActiveAdmin.register Payment do
 
 	menu priority: 8
 
+	index do
+    selectable_column
+    id_column    
+    column :user
+    column :receiver
+    column :formcomment
+    column :short_dest
+    column :sum
+    column :created_at
+    actions
+  end	
+
 	preserve_default_filters!
-	remove_filter :updated_at
+	remove_filter :quickpay_form, :targets, :payment_type, :label, :comment, :successURL, :need_fio,
+    			 			:need_email, :need_phone, :need_address, :updated_at
 
 end
